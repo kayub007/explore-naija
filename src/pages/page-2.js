3c5059f22bebe5b8d9d8937348faa9302b9ea000
kayub007 <kayub007@yahoo.com>
2021-01-23 12:48:24 +0100

    fourt commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,8 +8,8 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>Welcome to page 2 for now</p>
+    <Link to="/">Go back to the homepage from here and forth</Link>
   </Layout>
 )
 
